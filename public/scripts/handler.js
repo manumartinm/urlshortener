80f2d31c06394bbcf0ecd5942d25f978bb7add5d
Manuel Martín Morante <63638047+Manu200476@users.noreply.github.com>
2021-08-25 12:59:10 +0200

    Put handler funciton in a differnet file

---
+++ public/scripts/handler.js
@@ -0,0 +1,24 @@
+const handler = async () => {
+    try {
+        const longUrl = document.querySelector('#hero-field').value
+        const shortUrlParagraph = document.querySelector('#short-url')
+    
+        const req = await fetch('api/shorten', {
+            method: 'POST',
+            body: JSON.stringify({ longUrl }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((res) => res.json())
+    
+        shortUrlParagraph.innerHTML = req.shortUrl
+
+        return { longUrl, req }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+document.querySelector('#button-submit').addEventListener('click', handler)
+
+module.exports = handler
