0dd353ddc6dd8b2e151df3700149b34746c252a3
Manuel Martín Morante <63638047+Manu200476@users.noreply.github.com>
2021-08-25 12:33:00 +0200

    Change test for it

--- tests/api.test.js
+++ tests/api.test.js
@@ -18,31 +18,31 @@ const shortURL = async (longUrl) => {
 }
 
 describe('Test api', () => {
-    test('Expected to return json with long Url property', async () => {
+    it('Expected to return json with long Url property', async () => {
         const res = await shortURL('https://nike.com')
 
         expect(res).toHaveProperty('longUrl')
     })
 
-    test('Expected to throw error when parsing a url without https?', async () => {
+    it('Expected to throw error when parsing a url without https?', async () => {
         const res = await shortURL('nike.com')
 
         expect(res).toEqual(expect.stringMatching('La url es invalida'))
     })
 
-    test('Expected to throw error when parsing a url without tld', async () => {
+    it('Expected to throw error when parsing a url without tld', async () => {
         const res = await shortURL('https://nike')
 
         expect(res).toEqual(expect.stringMatching('La url es invalida'))
     })
 
-    test('Expect url code to be valid', async () => {
+    it('Expect url code to be valid', async () => {
         const res = await shortURL('https://nike.com')
 
         expect(res.urlCode).toMatch(new RegExp('[A-Za-z0-9]{9}'))
     })
 
-    test('Short Url domain be equal to website domain', async () => {
+    it('Short Url domain be equal to website domain', async () => {
         const res = await shortURL('https://nike.com')
 
         const code = res.shortUrl.split('/')[0]
@@ -50,10 +50,10 @@ describe('Test api', () => {
         expect(code).toEqual(BASE_URL)
     })
 
-    test('Expected to throw error when not parsing api key', async () => {
+    it('Expected to throw error when not parsing api key', async () => {
         const req = await fetch(shorten, {
             method: 'POST',
-            body: JSON.stringify({ longUrl }),
+            body: JSON.stringify({longUrl: 'https://nike.com' }),
             headers: {
                 'Content-Type': 'application/json',
             },
